453073163addd0ec1c93af7e6b035f735abb08d1
neuberfran <neuberfran@gmail.com>
2023-05-10 22:08:42 -0300

    folder app zephyr project here

--- app/src/main.c
+++ app/src/main.c
@@ -1,12 +1,76 @@
-/*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+#include <zephyr.h>
+#include <remoteproc.h>
+#include <device.h>
+#include <drivers/gpio.h>
 
-#include <zephyr/kernel.h>
+/* Remote processor handle */
+static struct rproc remote_proc;
 
-void main(void)
+/* LED GPIO pin */
+#define LED_GPIO_PIN 16  // Replace with your LED GPIO pin number
+
+/* Remote processor operations */
+static struct rproc_ops my_rproc_ops = {
+    .start = my_rproc_start,
+    .stop = my_rproc_stop,
+    .kick = my_rproc_kick,
+};
+
+/* GPIO device */
+static struct device *led_gpio_dev;
+
+/* Implementation of the start function */
+int my_rproc_start(struct rproc *rproc)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+    /* Power on and boot the remote processor */
+    /* Replace with your actual implementation */
+    /* Return 0 on success, a negative value on failure */
+    printk("Starting remote processor...\n");
+    // Your implementation here
+
+    /* Initialize LED GPIO pin */
+    led_gpio_dev = device_get_binding("GPIO_LED_DEVICE_NAME");  // Replace with your LED GPIO device name
+    if (!led_gpio_dev) {
+        printk("Failed to get LED GPIO device\n");
+        return -ENODEV;
+    }
+    int ret = gpio_pin_configure(led_gpio_dev, LED_GPIO_PIN, GPIO_OUTPUT_ACTIVE | FLAGS);  // Replace FLAGS with your desired GPIO flags
+    if (ret < 0) {
+        printk("Failed to configure LED GPIO pin\n");
+        return ret;
+    }
+
+    /* Blink LED */
+    while (1) {
+        gpio_pin_set(led_gpio_dev, LED_GPIO_PIN, 0);  // Turn LED ON
+        k_sleep(K_MSEC(500));  // Delay for 500ms
+        gpio_pin_set(led_gpio_dev, LED_GPIO_PIN, 1);  // Turn LED OFF
+        k_sleep(K_MSEC(500));  // Delay for 500ms
+    }
+
+    return 0;
+}
+
+/* Implementation of the stop function */
+int my_rproc_stop(struct rproc *rproc)
+{
+    /* Power off the remote processor */
+    /* Replace with your actual implementation */
+    /* Return 0 on success, a negative value on failure */
+    printk("Stopping remote processor...\n");
+    // Your implementation here
+
+    /* Turn off LED */
+    gpio_pin_set(led_gpio_dev, LED_GPIO_PIN, 1);
+
+    return 0;
+}
+
+/* Implementation of the kick function */
+void my_rproc_kick(struct rproc *rproc, int vqid)
+{
+    /* Kick the specified virtqueue */
+    /* Replace with your actual implementation */
+    printk("Kicking virtqueue %d...\n", vqid);
+    // Your implementation here
 }
