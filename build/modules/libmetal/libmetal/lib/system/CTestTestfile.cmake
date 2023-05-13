3c5d82b17ca27d5e8b50eb946d01483e398e62b7
neuberfran <neuberfran@gmail.com>
2023-05-13 13:12:52 -0300

    seventh

---
+++ build/modules/libmetal/libmetal/lib/system/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/neuberfran/zephyr_imx7d/modules/hal/libmetal/libmetal/lib/system
+# Build directory: /home/neuberfran/zephyr_imx7d/zephyr/boards/arm/pico_pi_m4/build/modules/libmetal/libmetal/lib/system
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("zephyr")
---
+++ build/modules/open-amp/open-amp/lib/virtio/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/neuberfran/zephyr_imx7d/modules/lib/open-amp/open-amp/lib/virtio
+# Build directory: /home/neuberfran/zephyr_imx7d/zephyr/boards/arm/pico_pi_m4/build/modules/open-amp/open-amp/lib/virtio
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
